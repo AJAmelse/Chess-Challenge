3dfbf68cfe2e07868b3bf55029129eaa8dec1239
AJAmelse <117790563+AJAmelse@users.noreply.github.com>
2024-04-08 15:07:36 -0500

    started implementation based on tinybot

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -2,8 +2,19 @@
 
 public class MyBot : IChessBot
 {
+    (ulong, Move, int, int, byte)[] TT = new (ulong, Move, int, int, byte)[2097152];
+
     public Move Think(Board board, Timer timer)
     {
+        Move rBestMove = default;
+
+    var (killers, allocatedTime, i, score, depth) = (new Move[256], timer.MillisecondsRemaining / 8, 0, 0, 1);
+
+ int Search(int ply, int depth, int alpha, int beta, bool nullAllowed)
+        {
+            
+        }
+
         Move[] moves = board.GetLegalMoves();
         return moves[0];
     }
