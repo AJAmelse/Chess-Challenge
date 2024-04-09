e622ac00cb45b632664336137ba7923b9522ccad
AJAmelse <117790563+AJAmelse@users.noreply.github.com>
2024-04-09 14:55:02 -0500

    Restarted Negamax function

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,18 +1,28 @@
-﻿using ChessChallenge.API;
+﻿using System.Security.Cryptography.X509Certificates;
+using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
-    (ulong, Move, int, int, byte)[] TT = new (ulong, Move, int, int, byte)[2097152];
+    
 
     public Move Think(Board board, Timer timer)
     {
+        Move[] allMoves = board.GetLegalMoves();
         Move rBestMove = default;
 
     var (killers, allocatedTime, i, score, depth) = (new Move[256], timer.MillisecondsRemaining / 8, 0, 0, 1);
 
- int Search(int ply, int depth, int alpha, int beta, bool nullAllowed)
+    Move Negamax(int Depth)
         {
-            
+            if (Depth == 0){
+                rBestMove = allMoves[1];
+                return rBestMove;
+            } 
+
+            int max = -2000000000;
+            foreach(Move move in allMoves){
+                
+            }
         }
 
         Move[] moves = board.GetLegalMoves();
